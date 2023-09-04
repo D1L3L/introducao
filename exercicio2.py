63d94c7d45fa92a0f1bf1589686bbe10059d35ff
Aquila Daniel Fernandes de Oliveira Flor <Dilel@academicos.com>
2023-09-04 20:01:12 -0300

    adicionando format na string no print

--- exercicio2.py
+++ exercicio2.py
@@ -2,7 +2,9 @@
 nota1 = float(input("Insira a primeira nota do aluno: "))
 nota2 = float(input("Insira a segundaa nota do aluno: "))
 media = (nota1 + nota2)/2
-if media >= 7:
-    print("Parabéns, o aluno foi aprovado com média",media)
+if 10 >= media >= 6:
+    print(f"Parabéns, o aluno foi aprovado com média {media}")
+elif 0 > media > 10:
+    print("Acho que você digitou algum valor errado, digite nnovamente")
 else:
-    print("Que pena o aluno teve de média", media)
+    print(f"Que pena o aluno teve de média {media}")
